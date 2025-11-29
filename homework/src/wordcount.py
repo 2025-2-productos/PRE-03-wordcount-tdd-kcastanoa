ec0bdfafdcbf0039369609f7368deec4acdc51ad
kcastanoa <kcastanoa@unal.edu.co>
2025-11-29 12:41:00 -0500

    Carga de todas las funciones

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,7 +1,3 @@
-# Ejemplo para casos de uso
-# py -m homework data/input data/output
-
-
 import argparse
 
 from homework.src._internals.read_all_lines import read_all_lines
@@ -23,6 +19,26 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
+def preprocess_lines(lines):
+    pass
+
+
+def split_into_words(preprocessed_lines):
+    pass
+
+
+def count_words(words):
+    pass
+
+
+def write_word_counts(output_folder, word_counts):
+    pass
+
+
 def main():
-    input, output = parse_args()
+    input_folder, output_folder = parse_args()
     lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
